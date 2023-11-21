acefb8e19ef694cfc0629c0b5b7041413d3921b5
Alexey <fav1290@gmail.com>
2023-11-21 15:13:07 +0300

    Added ingredients model

--- db/models.py
+++ db/models.py
@@ -1,6 +1,19 @@
+import uuid
+import datetime
+import sqlalchemy
+from sqlalchemy.orm import Mapped, mapped_column
+
+
 from db import Base
 
 
 class Ingredient(Base):
     __tablename__ = 'ingredients'
-    
+    id: Mapped[uuid.UUID] = mapped_column(sqlalchemy.UUID, primary_key=True)
+    owner_id: Mapped[uuid.UUID] = mapped_column(sqlalchemy.UUID)
+    kcal: Mapped[float] = mapped_column(sqlalchemy.Float)
+    proteins: Mapped[float] = mapped_column(sqlalchemy.Float)
+    fats: Mapped[float] = mapped_column(sqlalchemy.Float)
+    carbohydrates: Mapped[float] = mapped_column(sqlalchemy.Float)
+    creator_id: Mapped[uuid.UUID] = mapped_column(sqlalchemy.UUID, nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(sqlalchemy.DateTime, nullable=False)
